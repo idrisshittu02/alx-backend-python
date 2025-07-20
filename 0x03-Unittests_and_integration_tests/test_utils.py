6e77b352556552609aef302b24761bbbcd3ad80f
ibrahimshittu <ibshittu01@gmail.com>
2025-07-20 12:43:00 +0100

    Add unit tests and testaccess

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -13,3 +13,15 @@ class TestAccessNestedMap(unittest.TestCase):
     ])
     def test_access_nested_map(self, nested_map, path, expected):
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+class TestAccessNestedMap(unittest.TestCase):
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b")),
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        with self.assertRaises(KeyError) as context:
+            access_nested_map(nested_map, path)
+
+        # Confirm that the raised KeyError contains the correct missing key
+        self.assertEqual(str(context.exception), f"'{path[-1]}'")
