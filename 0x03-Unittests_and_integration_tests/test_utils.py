76f528d8febedcdf894d7ef762b29c869ce6d57e
ibrahimshittu <ibshittu01@gmail.com>
2025-07-20 12:53:23 +0100

    Add unit tests and testaccess2 Parameterize and patch

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -4,6 +4,7 @@ from parameterized import parameterized
 from unittest.mock import patch, Mock
 from utils import access_nested_map  # this function is already provided
 from utils import get_json
+from utils import memoize
 
 class TestAccessNestedMap(unittest.TestCase):
     """Test the access_nested_map function"""
@@ -43,4 +44,25 @@ class TestGetJson(unittest.TestCase):
 
             mock_get.assert_called_once_with(test_url)
             self.assertEqual(result, test_payload)
+
+class TestMemoize(unittest.TestCase):
+
+    def test_memoize(self):
+        class TestClass:
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch.object(TestClass, 'a_method', return_value=42) as mock_method:
+            test_obj = TestClass()
+            result1 = test_obj.a_property
+            result2 = test_obj.a_property
+
+            self.assertEqual(result1, 42)
+            self.assertEqual(result2, 42)
+            mock_method.assert_called_once()
+
 
