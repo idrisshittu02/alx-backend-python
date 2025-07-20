0f12b244df2d1ae79585369cc9b694eea6feb363
ibrahimshittu <ibshittu01@gmail.com>
2025-07-20 12:48:48 +0100

    Add unit tests and testaccess2

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 import unittest
 from parameterized import parameterized
+from unittest.mock import patch, Mock
 from utils import access_nested_map  # this function is already provided
+from utils import get_json
 
 class TestAccessNestedMap(unittest.TestCase):
     """Test the access_nested_map function"""
@@ -25,3 +27,20 @@ class TestAccessNestedMap(unittest.TestCase):
 
         # Confirm that the raised KeyError contains the correct missing key
         self.assertEqual(str(context.exception), f"'{path[-1]}'")
+        
+class TestGetJson(unittest.TestCase):
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+    ])
+    def test_get_json(self, test_url, test_payload):
+        mock_response = Mock()
+        mock_response.json.return_value = test_payload
+
+        with patch('utils.requests.get', return_value=mock_response) as mock_get:
+            result = get_json(test_url)
+
+            mock_get.assert_called_once_with(test_url)
+            self.assertEqual(result, test_payload)
+
