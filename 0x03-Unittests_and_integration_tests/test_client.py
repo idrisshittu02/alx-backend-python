845599832b7275a474a73d97b5f8f9fe61bacd36
ibrahimshittu <ibshittu01@gmail.com>
2025-07-20 15:57:46 +0100

    Add test public repos url client mocking

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -2,7 +2,7 @@
 """Unit tests for client module"""
 
 import unittest
-from unittest.mock import patch, PropertyMock
+from unittest.mock import patch, Mock, PropertyMock
 from parameterized import parameterized
 from client import GithubOrgClient
 
@@ -39,3 +39,21 @@ class TestGithubOrgClient(unittest.TestCase):
             result = client._public_repos_url
             expected = "https://api.github.com/orgs/test_org/repos"
             self.assertEqual(result, expected)
+    @patch('client.get_json')
+    def test_public_repos(self, mock_get_json):
+        """Test that public_repos returns the expected list of repo names"""
+        test_payload = [
+            {"name": "repo1"},
+            {"name": "repo2"},
+            {"name": "repo3"}
+        ]
+        mock_get_json.return_value = test_payload
+
+        with patch.object(GithubOrgClient, "_public_repos_url", new_callable=PropertyMock) as mock_url:
+             mock_url.return_value = "http://mocked_url"
+             client = GithubOrgClient("test_org")
+             result = client.public_repos()
+
+             self.assertEqual(result, ["repo1", "repo2", "repo3"])
+             mock_url.assert_called_once()
+             mock_get_json.assert_called_once_with("http://mocked_url")
