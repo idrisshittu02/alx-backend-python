74d5c2e056c10caea1d70e098b578403ccb91d61
ibrahimshittu <ibshittu01@gmail.com>
2025-07-20 20:45:49 +0100

    Add api endpoints corrected

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
+from rest_framework import routers
 from .views import ConversationViewSet, MessageViewSet
 
-router = DefaultRouter()
+# ✅ Explicitly define routers.DefaultRouter()
+router = routers.DefaultRouter()
 router.register(r'conversations', ConversationViewSet, basename='conversations')
 router.register(r'messages', MessageViewSet, basename='messages')
 
